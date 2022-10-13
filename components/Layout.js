320aa799f50e63970bec3ff0ce2e6844cf6c6371
jeongseockhyeon <jeong9260@naver.com>
2022-10-13 12:12:35 +0900

    main

--- components/Layout.js
+++ components/Layout.js
@@ -26,7 +26,7 @@ export default function Layout({ title, children }) {
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100 text-black">
           <p>Copyright &copy; 2022 NextShop</p>
         </footer>
       </div>
