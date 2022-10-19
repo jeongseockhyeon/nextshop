0011a72a00a0e7a53a1eddd712807f275c6f5e45
jeongseockhyeon <jeong9260@naver.com>
2022-10-19 18:30:25 +0900

    first

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProductItem({ product }) {
   return (
     <div className="card">
-      <Link href={'/product/${product.slug}'}>
+      <Link href={`/product/${product.slug}`}>
         <a>
           <img
             src={product.image}
@@ -14,7 +14,7 @@ export default function ProductItem({ product }) {
         </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
-        <Link href={'/product/${product.slug}'}>
+        <Link href={`/product/${product.slug}`}>
           <a>
             <h2 className="text-lg">{product.name}</h2>
           </a>
